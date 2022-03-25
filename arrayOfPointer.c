14d99748b1e701cbab4305412bdacbe28362bbf3
Dima Paryshkov <dima.paryshkov45@mail.ru>
2022-03-25 17:10:11 +0700

    Fix(arrayOfPointer.c): change some pointers

--- arrayOfPointer.c
+++ arrayOfPointer.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <malloc.h>
 
-int** dividingAnArrayIntoParts(int* inputArray)
+int** dividingAnArrayIntoParts(int* inputArray, const int* lenght, int* resultLenght)
 {
-	if ((sizeof(inputArray) / sizeof(int)) == 1)
+	if (*lenght == 1)
 		return NULL;
 
-	int resultLenght = 1;
-	int** result = (int**)malloc(sizeof(int*) * resultLenght);
+
+	*resultLenght = 1;
+	int** result = (int**)malloc(sizeof(int*) * *resultLenght);
+	int maxLenghtResult = 1;
 
 	int currentLenght = 1;
 	int LenghtNIV = 1;
 	int startIndex;
 	int* nonIncreasingValues = (int*)malloc(sizeof(int) * LenghtNIV);
+	int maxLenghtNIR = 1;
 
-	for (int i = 0; i < sizeof(inputArray) / sizeof(int) - 1; i++)
+	for (int i = 0; i < *lenght - 1; i++)
 	{
 		if (inputArray[i] < inputArray[i + 1])
 		{
@@ -27,38 +30,78 @@ int** dividingAnArrayIntoParts(int* inputArray)
 			if (currentLenght == 1)
 			{
 				nonIncreasingValues[0] = LenghtNIV++;
-				if ((LenghtNIV + 1) == (sizeof(nonIncreasingValues) / sizeof(int)))
-					nonIncreasingValues = (int*)realloc(nonIncreasingValues, sizeof(int) * 2 * LenghtNIV);
-				nonIncreasingValues[LenghtNIV] = inputArray[i];
+				if ((LenghtNIV + 1) >= maxLenghtNIR)
+				{
+					maxLenghtNIR = 2 * LenghtNIV;
+					nonIncreasingValues = (int*)realloc(nonIncreasingValues, sizeof(int) * maxLenghtNIR);
+				}
+				nonIncreasingValues[LenghtNIV - 1] = inputArray[i];
 			}
 			else
 			{
-				if ((resultLenght + 1) == (sizeof(result) / sizeof(int*)))
-					result = (int**)realloc(result, sizeof(int*) * 2 * resultLenght);
+				if ((*resultLenght + 1) >= maxLenghtResult)
+				{
+					maxLenghtResult = 2 * *resultLenght;
+					result = (int**)realloc(result, sizeof(int*) * maxLenghtResult);
+				}
 
-				result[resultLenght - 1] = (int*)malloc(sizeof(int) * (currentLenght + 1));
+				result[*resultLenght - 1] = (int*)malloc(sizeof(int) * (currentLenght + 1));
 
-				result[resultLenght - 1][0] = currentLenght;
+				result[*resultLenght - 1][0] = currentLenght;
 				for (int j = 0; j < currentLenght; j++)
-					result[resultLenght - 1][j + 1] = inputArray[startIndex + j];
+					result[*resultLenght - 1][j + 1] = inputArray[startIndex + j];
 
 				currentLenght = 1;
-				resultLenght++;
+				(*resultLenght)++;
 			}
 		}
-		result[resultLenght] = nonIncreasingValues;
-		currentLenght = 0;
 	}
+	if (currentLenght == 1)
+	{
+		nonIncreasingValues[0] = LenghtNIV++;
+		if ((LenghtNIV + 1) >= maxLenghtNIR)
+		{
+			maxLenghtNIR = 2 * LenghtNIV;
+			nonIncreasingValues = (int*)realloc(nonIncreasingValues, sizeof(int) * maxLenghtNIR);
+		}
+		nonIncreasingValues[LenghtNIV - 1] = inputArray[*lenght - 1];
+	}
+	else
+	{
+		if ((*resultLenght + 1) >= maxLenghtResult)
+		{
+			maxLenghtResult = 2 * *resultLenght;
+			result = (int**)realloc(result, sizeof(int*) * maxLenghtResult);
+		}
+
+		result[*resultLenght - 1] = (int*)malloc(sizeof(int) * (currentLenght + 1));
+
+		result[*resultLenght - 1][0] = currentLenght;
+		for (int j = 0; j < currentLenght; j++)
+			result[*resultLenght - 1][j + 1] = inputArray[startIndex + j];
+
+		currentLenght = 1;
+		(*resultLenght)++;
+	}
+	result[*resultLenght - 1] = nonIncreasingValues;
 
 	return result;
 }
 
 int main() 
 {
-	fprintf(stdout, "Please enter a sequence of integers separated by a space: \n");
+	fprintf(stdout, "Please enter a sequence of integers separated by a space. Enter -1 at the end of the sequence!\n");
 	
-	int* inputArray = NULL;
-	for (int i = 0; fscanf_s(stdin, "%d", inputArray[i]) != 0; i++)
-		inputArray = (int*)realloc(inputArray, sizeof(int) * (i + 1));
-	inputArray[0]++;
+	int* inputArray = (int*)malloc(sizeof(int));
+	int lenght = 0;
+
+	for (int tmp; 1; lenght++)
+	{
+		fscanf_s(stdin, "%d", &tmp);
+		if (tmp == -1)
+			break;
+		inputArray = (int*)realloc(inputArray, sizeof(int) * (lenght + 1));
+		inputArray[lenght] = tmp;
+	}
+
 }
